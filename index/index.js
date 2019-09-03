8c9bcd735e24b6b39dae48a80979c98a99ee7007
Josivanio Tadeu Nunes Leite Sobrinho <j.leite.sobrinho@avanade.com>
2019-09-03 11:42:52 -0300

    atividade encontro04

---
+++ index/index.js
@@ -0,0 +1,68 @@
+var listaTarefas = new Array();
+
+function criarTarefa() {
+    var nomeTarefa = document.getElementById('nomeTarefa').value;
+    var statusCompleto = document.getElementById('statusCompleto');
+
+    var retornoStatus = status(statusCompleto);
+
+    listaTarefas.push(nomeTarefa, retornoStatus);
+
+    console.log(listaTarefas);
+}
+
+function deletarTarefa() {
+    var nomeTarefa = document.getElementById('nomeTarefa').value;
+    var index = localizarIndice(nomeTarefa);
+
+    if (index != null) {
+        listaTarefas.splice(index, 2)
+    }
+
+    console.log(listaTarefas);
+}
+
+function alterarTarefa() {
+    var nomeAlteradoTarefa = document.getElementById('nomeAlteradoTarefa').value;
+    var nomeTarefa = document.getElementById('nomeTarefa').value;
+    var statusCompleto = document.getElementById('statusAlterarCompleto');
+    var retornoStatus = status(statusCompleto);
+
+    var index = localizarIndice(nomeTarefa);
+    listaTarefas.splice(index, 2);
+    listaTarefas.push(nomeTarefa, retornoStatus);
+    
+    console.log(listaTarefas);
+
+    // btnAlterar.value = 'Confirmar Alteração';
+    //voltar ao nome antigo dps de confirmar
+    // if (nomeAlteradoTarefa.style.display === "none") {
+    //     nomeAlteradoTarefa.style.display = "block";
+    //   } else {
+    //     nomeAlteradoTarefa.style.display = "none";
+    //   }
+}
+
+function consultarTarefas() {
+    var statusTarefa = document.getElementById('select');
+    statusTarefa = listaTarefas;    
+}
+
+
+function localizarIndice(name) {
+    for (let index = 0; index < listaTarefas.length; index++) {
+        if (listaTarefas[index] == name) {
+            return index;
+        }
+    }
+    return null;
+}
+
+function status(statusCompleto) {
+
+    if (statusCompleto.checked == true) {
+        return "Completo";
+    } else {
+        return "Não completo";
+    }
+}
